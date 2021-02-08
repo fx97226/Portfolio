90a3473010fbeea3c399bcc905e4b62062ca676d
Francois-xavier <francois-xavier.hilaire@efrei.net>
2021-02-08 14:32:06 +0100

    init

---
+++ full screen video/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+	$('.btn-menu').click(function(){
+		$('.nav .menu').toggleClass("active");
+		$('.btn-menu i').toggleClass("active");
+	});
+
+
+});
+
+$('.skill-per').each(function(){
+    var $this = $(this);
+    var per = $this.attr('per');
+    $this.css("width",per+'%');
+    $({animatedValue: 0}).animate({animatedValue: per},{
+      duration: 1000,
+      step: function(){
+        $this.attr('per', Math.floor(this.animatedValue) + '%');
+      },
+      complete: function(){
+        $this.attr('per', Math.floor(this.animatedValue) + '%');
+      }
+    });
+  });
